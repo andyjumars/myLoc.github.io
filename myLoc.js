38ea5b1a7afd22be2d362f4ee55e43dc693b9640
andyjumars <andyjumars@hotmail.com>
2017-01-29 02:39:15 +0800

    update
    update

--- myLoc.js
+++ myLoc.js
@@ -1,46 +1,26 @@
-var options = {enableHighAccuracy: ture,timeout:100,maximumAge:0};
+/* myLoc.js */
+
+var watchId = null;
 var map = null;
 var ourCoords =  {
-	latitude: 24.086697,
-	longitude: 120.712961
+	latitude: 47.624851,
+	longitude: -122.52099
 };
+var prevCoords = null;
 
 window.onload = getMyLocation;
 
 function getMyLocation() {
 	if (navigator.geolocation) {
-
-		navigator.geolocation.getCurrentPosition(
-			displayLocation, 
-			displayError);
+		var watchButton = document.getElementById("watch");
+		watchButton.onclick = watchLocation;
+		var clearWatchButton = document.getElementById("clearWatch");
+		clearWatchButton.onclick = clearWatch;
 	}
 	else {
 		alert("Oops, no geolocation support");
 	}
 }
-fuction displayError(error) {
-        var errorTypes = {
-            0: "Unkowon error",
-            1: "Permission denied",
-            2: "Position is not avalible",
-            3: "Request timeout"
-        };
-var errorMessage = errorTypes[error.code];
-if (error.code == 0 || error.code == 2) {
-	errorMessage = erroeMessage + ""+error.message;
-}
-var div = document.getElementById("location");
-div.innerHTML = errorMessage;
-option.timeout += 100;
-navigator.geolocation.getCurrentPosition(
-    displayLocation,
-    displayError,
-    options);
-div.innerHTML += "... checking again with timeout=" 
-+ options.timeout;
-}
-
-
 
 function displayLocation(position) {
 	var latitude = position.coords.latitude;
@@ -48,26 +28,38 @@ function displayLocation(position) {
 
 	var div = document.getElementById("location");
 	div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
+	div.innerHTML += " (with " + position.coords.accuracy + " meters accuracy)";
 
 	var km = computeDistance(position.coords, ourCoords);
 	var distance = document.getElementById("distance");
 	distance.innerHTML = "You are " + km + " km from the WickedlySmart HQ";
 
-	showMap(position.coords);
-div.innerHTML += "(foud in" + options.timeout+"millseconds)";
-
+	if (map == null) {
+		showMap(position.coords);
+		prevCoords = position.coords;
+	}
+	else {
+		var meters = computeDistance(position.coords, prevCoords) * 1000;
+		if (meters > 20) {
+			scrollMapToPosition(position.coords);
+			prevCoords = position.coords;
+		}
+	}
 }
 
 
-
-
+// --------------------- Ready Bake ------------------
+//
+// Uses the Spherical Law of Cosines to find the distance
+// between two lat/long points
+//
 function computeDistance(startCoords, destCoords) {
 	var startLatRads = degreesToRadians(startCoords.latitude);
 	var startLongRads = degreesToRadians(startCoords.longitude);
 	var destLatRads = degreesToRadians(destCoords.latitude);
 	var destLongRads = degreesToRadians(destCoords.longitude);
 
-	var Radius = 6371;
+	var Radius = 6371; // radius of the Earth in km
 	var distance = Math.acos(Math.sin(startLatRads) * Math.sin(destLatRads) + 
 					Math.cos(startLatRads) * Math.cos(destLatRads) *
 					Math.cos(startLongRads - destLongRads)) * Radius;
@@ -80,7 +72,7 @@ function degreesToRadians(degrees) {
 	return radians;
 }
 
-
+// ------------------ End Ready Bake -----------------
 
 function showMap(coords) {
 	var googleLatAndLong = new google.maps.LatLng(coords.latitude, 
@@ -93,11 +85,10 @@ function showMap(coords) {
 	var mapDiv = document.getElementById("map");
 	map = new google.maps.Map(mapDiv, mapOptions);
 
-	
+	// add the user marker
 	var title = "Your Location";
 	var content = "You are here: " + coords.latitude + ", " + coords.longitude;
 	addMarker(map, googleLatAndLong, title, content);
-
 }
 
 function addMarker(map, latlong, title, content) {
@@ -136,4 +127,33 @@ function displayError(error) {
 	var div = document.getElementById("location");
 	div.innerHTML = errorMessage;
 }
+
+//
+// Code to watch the user's location
+//
+function watchLocation() {
+	watchId = navigator.geolocation.watchPosition(
+					displayLocation, 
+					displayError);
+}
+
+function scrollMapToPosition(coords) {
+	var latitude = coords.latitude;
+	var longitude = coords.longitude;
+
+	var latlong = new google.maps.LatLng(latitude, longitude);
+	map.panTo(latlong);
+
+	// add the new marker
+	addMarker(map, latlong, "Your new location", "You moved to: " + 
+								latitude + ", " + longitude);
+}
+
+function clearWatch() {
+	if (watchId) {
+		navigator.geolocation.clearWatch(watchId);
+		watchId = null;
+	}
+}
+
 
